837523bb55f5d2b3bb51802e961eb0cc67937d11
mbunta <mbunta@unc.edu>
2025-12-08 00:06:13 -0500

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { api } from "@/utils/trpc/api";
 import Header from "@/components/header";
+import { Analytics } from "@vercel/analytics/react"; 
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -23,8 +24,11 @@ function App({ Component, pageProps }: AppProps) {
     >
       <Header />
       <Component {...pageProps} />
+
+      <Analytics />   
     </ThemeProvider>
   );
 }
 
 export default api.withTRPC(App);
+
